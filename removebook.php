3ae68746a04d9270aac2afb216b0f8213bdd23f7
Patryk129 <112767162+Patryk129@users.noreply.github.com>
2024-06-01 00:47:24 +0200

    Delete removebook.php

--- removebook.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'admin.php';
-    require 'connect.php';
-    $id = $_POST['id_ksiazki']; 
-    $query= "DELETE FROM ksiazka WHERE id_ksiazki = '$id' ";
-    $result = mysqli_query($conn,$query);
-    mysqli_close($conn);
-    header('ksiazka.php')
-    ?>
