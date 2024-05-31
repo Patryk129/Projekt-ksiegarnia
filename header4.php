65276879c69cbbad35e68a5aaf7392eec74682ec
Patryk129 <112767162+Patryk129@users.noreply.github.com>
2024-06-01 00:46:09 +0200

    Delete header4.php

--- header4.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <div class="container">
-        <h1>Księgarnia Internetowa - Administracja</h1>
-    </div>
-    <ul>
-        <li><a href="index.php">Glowna</a></li>
-        <li><a href="wyloguj.php">Wyloguj</a></li>
-    </ul>
-</header>
