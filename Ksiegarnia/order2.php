657602b602dae4cc1838a091174c3c258ea18e94
Patryk129 <112767162+Patryk129@users.noreply.github.com>
2024-06-02 23:52:58 +0200

    Update order2.php

--- Ksiegarnia/order2.php
+++ Ksiegarnia/order2.php
@@ -4,7 +4,7 @@
     // echo $id_klienta;
     $query = "UPDATE zamowieniee 
     SET zamowieniee.oplacone = True, zamowieniee.data_zlozenia_zamowienia = CURRENT_DATE() 
-    WHERE id_klienta = $id_klienta";
+    WHERE id_klienta = $id_klienta and zamowieniee.oplacone is null";
     $result = mysqli_query($conn,$query);
     mysqli_close($conn);
     header('location: order.php');
